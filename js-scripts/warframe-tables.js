f225ffbef20a44d709eff47013a3857096a1e86d
Jace Sherwood <coffeehuskblues@gmail.com>
2026-03-26 15:00:21 -0400

    Revert "Merge branch 'site' of https://github.com/CoffeeBlusel/coffeeblusel.github.io into site"
    This reverts commit 60b14b6d4d4cb85e88612e701420737e2e8679c7, reversing
    changes made to 0bccb02d461bfbf9bc294f444eb66688f2820644.

--- js-scripts/warframe-tables.js
+++ js-scripts/warframe-tables.js
@@ -3,9 +3,12 @@ const jsonFilePath = "/public/warframe_drops.json";
 $(document).ready(function() {
     // Print out categories loaded from JSON file // Debug
     $.getJSON(jsonFilePath, function(data) {
-        const categories = Object.keys(data);
-        console.log("Categories loaded from JSON file:", categories);
+        const categories = Object.keys(data);   // Get the categories (arrays) from the JSON data
+        const values = Object.values(data);     // Get the values (arrays of items) from the JSON data
+        const items = Object.entries(data);     // Get the entries (key-value pairs) from the JSON data
 
-        
+        console.log("Categories loaded from JSON file:", categories);
+        console.log("Values loaded from JSON file:", values);
+        console.log("Items loaded from JSON file:", items);
     });
 });
